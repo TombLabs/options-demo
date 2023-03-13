6fd72323ffebe4c5b81859fd23f68a99abe101da
Tomb Labs <117311234+TombLabs@users.noreply.github.com>
2023-03-13 02:15:51 +0200

    Quick update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Nav = () => {
   return (
     <div className="absolute top-0 w-full h-36 flex justify-end items-center px-8 border-2 border-neutral-500/50">
-      <img src="/hypertomb.png" alt="Hypertomb" className="w-32 mr-auto" />
+      <img src="/tensor.png" alt="Hypertomb" className="w-32 mr-auto" />
       <WalletMultiButton />
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="absolute top-36 left-0 h-full text-center w-48 flex justify-center items-start border-2 border-neutral-500/50 p-8">
-      Awesome Hyperspace Filter Stuff
+      I'm an awesome sidebar.
     </div>
   );
 };
